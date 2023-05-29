4c40a104166c88978a98e37107373d834648d37f
Geir Sørensen <geirs73@gmail.com>
2023-05-29 15:00:26 +0200

    Working example

--- src/static/gen.ps1
+++ src/static/gen.ps1
@@ -1,43 +1,71 @@
-[string] $saxonDecimalVersion = '12.2'
-[string] $saxonUrlVersion = $saxonDecimalVersion.Replace('.', '-')
-[int] $saxonMajorVersion = $($saxonUrlVersion -Split '-')[0]
-[string] $saxonName = "SaxonHE$($saxonUrlVersion)J"
-[string] $saxonLibFile = "saxon-he-$($saxonDecimalVersion).jar"
-[string] $saxonLibDir = Join-Path '..' 'lib'
-[string] $saxonLib = Join-Path $saxonLibDir $saxonLibFile
+[string] $SaxonLib
 
-[string] $saxonZip = "$($saxonName).zip"
-[string] $saxonUrl = "https://github.com/Saxonica/Saxon-HE/raw/main/$($saxonMajorVersion)/Java"
+function TransformToHtml {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $XmlInputFile,
 
-[string] $tempDir = Join-Path '..' 'temp'
-[string] $unzipArea = Join-Path $tempDir $saxonName
-[string] $dlPath = Join-Path $tempDir $saxonZip
+        [Parameter(Mandatory = $true)]
+        [string] $TemplateFile,
+
+        [Parameter(Mandatory = $true)]
+        [string] $HtmlOutputFile
+    )
+    $(java -jar $SaxonLib -s:"$XmlInputFile" -xsl:"$TemplateFile" -o:"$HtmlOutputFile")
+}
+
+function GetSaxon {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string] $Version = '12.2'
+    )
+
+    [string] $SaxonDecimalVersion = $Version
+    [string] $SaxonLibFile = "saxon-he-$($SaxonDecimalVersion).jar"
+    [string] $SaxonLibDir = Join-Path '..' 'lib'
+    [string] $SaxonLib = Join-Path $SaxonLibDir $SaxonLibFile
+    [string] $SaxonUrlVersion = $SaxonDecimalVersion.Replace('.', '-')
+    [int] $SaxonMajorVersion = $($SaxonUrlVersion -Split '-')[0]
+    [string] $SaxonName = "SaxonHE$($SaxonUrlVersion)J"
+    [string] $SaxonZip = "$($SaxonName).zip"
+    [string] $SaxonUrl = "https://github.com/Saxonica/Saxon-HE/raw/main/$($SaxonMajorVersion)/Java"
+    [string] $tempDir = Join-Path '..' 'temp'
+    [string] $unzipArea = Join-Path $tempDir $SaxonName
+    [string] $dlPath = Join-Path $tempDir $SaxonZip
+
+    if (Test-Path -Path $SaxonLib -PathType Leaf) {
+        return $SaxonLib
+    }
+
+
+    Remove-Item -Recurse -Force -Path $tempDir | Out-Null
+
+    if (!(Test-Path $unzipArea -PathType Container)) {
+        New-Item -ItemType Directory $unzipArea | Out-Null
+    }
+
+    if (!(Test-Path $SaxonLibDir -PathType Container)) {
+        New-Item -ItemType Directory $SaxonLibDir | Out-Null
+    }
+
+    Invoke-WebRequest -Uri "$SaxonUrl/$SaxonZip" -OutFile $dlPath | Out-Null
+    Expand-Archive -Path $dlPath -DestinationPath $unzipArea | Out-Null
+    Copy-Item $(Join-Path $unzipArea $SaxonLibFile) $SaxonLib | Out-Null
+    Copy-Item $(Join-Path $unzipArea 'lib') $SaxonLibDir -Recurse | Out-Null
+
+    return $SaxonLib
+}
 
 function Main() {
-    if (!(Test-Path $saxonLib))
-    {
-        DownloadSaxon
-    }
-     $(java -jar $saxonLib --version)
+
+    # $(java -jar $SaxonLib -?)
+    TransformToHtml `
+        -TemplateFile '.\html\ordensregler.xslt' `
+        -HtmlOutputFile '.\ordensregler.html' `
+        -XmlInputFile '.\ordensregler.xml'
 }
 
-function DownloadSaxon() {
-    Remove-Item -Recurse -Force -Path $tempDir
-
-    if (!(Test-Path $unzipArea -PathType Container))
-    {
-        New-Item -ItemType Directory $unzipArea
-    }
-
-    if (!(Test-Path $saxonLibDir -PathType Container))
-    {
-        New-Item -ItemType Directory $saxonLibDir
-    }
-
-    Invoke-WebRequest -Uri "$saxonUrl/$saxonZip" -OutFile $dlPath
-    Expand-Archive -Path $dlPath -DestinationPath $unzipArea
-    Copy-Item $(Join-Path $unzipArea $saxonLibFile) $saxonLib
-}
+[string] $SaxonLib = GetSaxon -Version 12.2
 
 Main
 
