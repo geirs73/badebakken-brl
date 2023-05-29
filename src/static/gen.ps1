18db0ca35279fbf3121065c107cc241e3c94a37d
Geir Sørensen <geirs73@gmail.com>
2023-05-29 14:00:22 +0200

    Generate script now download saxon if not present

--- src/static/gen.ps1
+++ src/static/gen.ps1
@@ -1,22 +1,47 @@
-$saxonHeVersion = "12.1"
-$saxonVersion = $saxonHeVersion.Replace('.', '-')
-$saxonZip = "SaxonHE$($saxonVersion)J.zip"
-$saxonUrl = 'https://github.com/Saxonica/Saxon-HE/raw/main/12/Java'
-$saxonFullUrl = "$saxonUrl/$saxonZip"
-$unzipArea = Join-Path '..' 'temp'
-$saxonPath = Join-Path '..' 'lib' 'saxon-he-12.1.jar'
-$dlPath = Join-Path $unzipArea $unzipArea $saxonZip
+[string] $saxonDecimalVersion = '12.2'
+[string] $saxonUrlVersion = $saxonDecimalVersion.Replace('.', '-')
+[int] $saxonMajorVersion = $($saxonUrlVersion -Split '-')[0]
+[string] $saxonName = "SaxonHE$($saxonUrlVersion)J"
+[string] $saxonLibFile = "saxon-he-$($saxonDecimalVersion).jar"
+[string] $saxonLibDir = Join-Path '..' 'lib'
+[string] $saxonLib = Join-Path $saxonLibDir $saxonLibFile
 
-if (!(Test-Path $unzipArea -PathType Container))
-{
-    New-Item -ItemType Directory $unzipArea
+[string] $saxonZip = "$($saxonName).zip"
+[string] $saxonUrl = "https://github.com/Saxonica/Saxon-HE/raw/main/$($saxonMajorVersion)/Java"
+
+[string] $tempDir = Join-Path '..' 'temp'
+[string] $unzipArea = Join-Path $tempDir $saxonName
+[string] $dlPath = Join-Path $tempDir $saxonZip
+
+function Main() {
+    if (!(Test-Path $saxonLib))
+    {
+        DownloadSaxon
+    }
+     $(java -jar $saxonLib --version)
 }
 
-Invoke-WebRequest -Uri $saxonFullUrl -OutFile $dlPath
-Expand-Archive -Path $dlPath -DestinationPath $unzipArea
-Copy-Item
+function DownloadSaxon() {
+    Remove-Item -Recurse -Force -Path $tempDir
+
+    if (!(Test-Path $unzipArea -PathType Container))
+    {
+        New-Item -ItemType Directory $unzipArea
+    }
+
+    if (!(Test-Path $saxonLibDir -PathType Container))
+    {
+        New-Item -ItemType Directory $saxonLibDir
+    }
+
+    Invoke-WebRequest -Uri "$saxonUrl/$saxonZip" -OutFile $dlPath
+    Expand-Archive -Path $dlPath -DestinationPath $unzipArea
+    Copy-Item $(Join-Path $unzipArea $saxonLibFile) $saxonLib
+}
+
+Main
+
 
 # https://github.com/Saxonica/Saxon-HE/raw/main/12/Java/SaxonHE12_1J.zip
-# https://github.com/Saxonica/Saxon-HE/raw/main/12/Java/SaxonHE12-1J.zip
 
 
